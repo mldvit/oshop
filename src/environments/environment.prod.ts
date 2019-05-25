f9bbc330b0e48da65262d18e1ca99679b5762ced
Della Vedova Maria Luisa <IT244@itad.infotn.it>
2019-05-25 17:02:01 +0200

    oshop set up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAAnsI_29QvEsPwIlO51t_6DkGbS8tzxko",
+    authDomain: "mlshop.firebaseapp.com",
+    databaseURL: "https://mlshop.firebaseio.com",
+    projectId: "mlshop",
+    storageBucket: "mlshop.appspot.com",
+    messagingSenderId: "1087178767358",
+    appId: "1:1087178767358:web:23e4c036880f90d8"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAAnsI_29QvEsPwIlO51t_6DkGbS8tzxko",
+    authDomain: "mlshop.firebaseapp.com",
+    databaseURL: "https://mlshop.firebaseio.com",
+    projectId: "mlshop",
+    storageBucket: "mlshop.appspot.com",
+    messagingSenderId: "1087178767358",
+    appId: "1:1087178767358:web:23e4c036880f90d8"
+  }
 };
 
 /*
