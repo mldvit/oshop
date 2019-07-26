b7fa7e3af3bd3ca65579533a3b8a15137acfb39c
Della Vedova Maria Luisa <IT244@itad.infotn.it>
2019-07-26 17:17:10 +0200

    logut + displayName

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.scss']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent  {
 
-  constructor() { }
+  user$: Observable<firebase.User>;
 
-  ngOnInit() {
+  constructor(public afAuth: AngularFireAuth) {
+    this.user$ = this.afAuth.authState;
+    console.log(this.user$);
+   }
+
+  logOut() {
+    this.afAuth.auth.signOut();
   }
 
 }
