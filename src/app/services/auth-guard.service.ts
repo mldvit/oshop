1cf70658dd102b08b575e5aec7a2fd67e9a94627
Della Vedova Maria Luisa <IT244@itad.infotn.it>
2019-07-27 16:15:50 +0200

    fix  wrong import CanActivate

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
-import { CanActivate } from '@angular/router/src/utils/preactivation';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, CanActivate } from '@angular/router';
 import { map } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 
@@ -8,13 +7,10 @@ import { AuthService } from './auth.service';
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthGuard implements CanActivate {
 
-  route: ActivatedRouteSnapshot;
-  path: ActivatedRouteSnapshot[];
-
-  constructor( private auth: AuthService,
-               private router: Router) { }
+  constructor( private auth: AuthService, private router: Router) { }
 
     canActivate( route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
       return this.auth.user$.pipe( map(user => {
