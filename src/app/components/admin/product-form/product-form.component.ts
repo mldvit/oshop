9b01aa2118206c6e90c8f288d19e5a3326dd6d48
Della Vedova Maria Luisa <IT244@itad.infotn.it>
2019-08-10 17:48:10 +0200

    8- Rendering the List of Products 9- Editing a Product

--- src/app/components/admin/product-form/product-form.component.ts
+++ src/app/components/admin/product-form/product-form.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CategoryService } from 'src/app/services/category.service';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-form',
@@ -13,9 +14,20 @@ import { Router } from '@angular/router';
 export class ProductFormComponent implements OnInit {
 
    categories$;
+   product: Product = {};
+   productId;
 
-  constructor(private categoriesService: CategoryService, private productService: ProductService, private router: Router) {
-    this.categories$ = categoriesService.getCategories();
+  constructor(private categoriesService: CategoryService,
+              private productService: ProductService,
+              private router: Router,
+              private route: ActivatedRoute) {
+                this.categories$ = categoriesService.getCategories();
+                this.productId = route.snapshot.paramMap.get('id');
+                if (this.productId) {
+                  // productService.get(this.productId).subscribe(p => this.product = p);  
+                  // take prende solo il primo valore e unsbuscribe in automatico
+                  productService.get(this.productId).pipe(take(1)).subscribe(p => this.product = p);
+                }
   }
 
   ngOnInit() {
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Product } from '../models/product';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +13,20 @@ export class ProductService {
 
   create( product: Product ) {
     return this.db.list('products').push(product);
- //   return this.db.object('/products').update(product);
   }
 
+  getAll() {
+    return this.db.list<Product>('/products', ref =>
+      ref.limitToLast(25).orderByChild('title')).snapshotChanges().pipe(map(actions =>
+        actions.map(a => ({ key: a.key, ...a.payload.val() }))
+      ));
+  }
+
+  get(productId): Observable<Product> {
+/*     return this.db.list<Product>('/products/' + productId, ref =>
+    ref.limitToLast(1)).snapshotChanges().pipe(map(actions =>
+      actions.map(a => ({ key: a.key, ...a.payload.val() }))[0]
+    )); */
+    return this.db.object<Product>('/products/' + productId).valueChanges();
+  }
 }
