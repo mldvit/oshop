2eeb2af5462d6e64bef6c39b0f239c710aa52dd3
Della Vedova Maria Luisa <IT244@itad.infotn.it>
2019-09-22 22:57:16 +0200

    fitering products by category first step

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ProductService } from 'src/app/services/product.service';
 import { CategoryService } from 'src/app/services/category.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-products',
@@ -10,12 +12,24 @@ import { CategoryService } from 'src/app/services/category.service';
 })
 export class ProductsComponent {
 
-  books$: Observable<any>;
+  books: Product[] = [];
+  filteredBooks: Product[] = [];
   categories$: Observable<any>;
+  category: string;
 
-  constructor(private productService: ProductService, private categoryService: CategoryService) {
-    this. books$ = productService.getAll();
+  constructor(route: ActivatedRoute,
+              private productService: ProductService, 
+              private categoryService: CategoryService) {
+    productService.getAll().subscribe(pp => {
+      return this.books = this.filteredBooks = pp;
+    });
     this.categories$ = categoryService.getAll();
+
+    route.queryParamMap.subscribe(params => {
+      this.category = params.get('category');
+      this.filteredBooks = this.category ? this.books.filter(b => b.category === this.category) : this.books;
+    });
+
    }
 
 }
