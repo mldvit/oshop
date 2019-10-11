2baacf719f002e33ec0dd3ba2cd000e2061d27ae
Della Vedova Maria Luisa <IT244@itad.infotn.it>
2019-10-11 15:52:25 +0200

    work in progress studying rxjs to fix

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { ProductService } from 'src/app/services/product.service';
-import { CategoryService } from 'src/app/services/category.service';
-import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable, forkJoin } from 'rxjs';
+import {map, switchMapTo} from 'rxjs/operators';
 import { Product } from 'src/app/models/product';
+import { CategoryService } from 'src/app/services/category.service';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-products',
@@ -17,10 +18,14 @@ export class ProductsComponent {
   categories$: Observable<any>;
   category: string;
 
-  constructor(route: ActivatedRoute,
-              private productService: ProductService, 
-              private categoryService: CategoryService) {
-    productService.getAll().subscribe(pp => {
+  constructor(private route: ActivatedRoute,
+              private productService: ProductService,
+              private categoryService: CategoryService) {}
+  
+  ngOnInit() {
+    this.categories$ = this.categoryService.getAll();
+
+  /*     productService.getAll().subscribe(pp => {
       return this.books = this.filteredBooks = pp;
     });
     this.categories$ = categoryService.getAll();
@@ -28,8 +33,39 @@ export class ProductsComponent {
     route.queryParamMap.subscribe(params => {
       this.category = params.get('category');
       this.filteredBooks = this.category ? this.books.filter(b => b.category === this.category) : this.books;
-    });
+    }); */
 
-   }
+/*     console.log('1)');
+    forkJoin(
+      productService.getAll(),
+      route.queryParamMap
+    ).subscribe(res => {
+      console.log('2)');
+      this.books = res[0];
+      this.category = res[1].get('category');
+      this.filteredBooks = this.category ? this.books.filter(b => b.category === this.category) : this.books;
+      console.log(this.books);
+    }
+    ); */
 
+/*     productService.getAll().subscribe(pp => {
+      this.books = this.filteredBooks = pp;
+      route.queryParamMap.subscribe(params => {
+        this.category = params.get('category');
+        this.filteredBooks = this.category ? this.books.filter(b => b.category === this.category) : this.books;
+      });
+    }); */
+/* 
+    https://www.learnrxjs.io/operators/transformation/switchmap.html
+    https://medium.com/@paynoattn/3-common-mistakes-i-see-people-use-in-rx-and-the-observable-pattern-ba55fee3d031
+    this.appParameters
+    .map(params => params['id'])
+    .switchMap(id => {
+      if(id !== null && id !== undefined) { 
+        return this.getUser(id)
+      }
+    })
+    .subscribe(user => this.user = user); */
+
+  }
 }
