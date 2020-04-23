61dd590b6318f568297537dd78bd1bfaa4146bce
Della Vedova Maria Luisa <TD244@tndigit.local>
2020-04-23 16:15:36 +0200

    4- Saving the Order in Firebase

--- src/app/components/check-out/check-out.component.ts
+++ src/app/components/check-out/check-out.component.ts
@@ -1,14 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+import { ShoppingCart } from 'src/app/models/shopping-cart.model';
+import { Subscription } from 'rxjs';
+import { OrderService } from 'src/app/services/order.service';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.sass']
 })
-export class CheckOutComponent {
+export class CheckOutComponent implements OnInit, OnDestroy {
   shipping = {};
+  shoppingCart: ShoppingCart;
+  subscription: Subscription;
+
+  constructor(private cartService: CartService, private orderService: OrderService) { }
+
+  ngOnInit() {
+    this.subscription = this.cartService.getCart().subscribe((cart) => this.shoppingCart = cart);
+   }
 
   placeOrder() {
-    console.log(this.shipping);
+    const order = {
+      datePlaced: new Date().getTime(),
+      shipping: this.shipping,
+      items: this.shoppingCart.items.map(i => {
+        return {
+          product: {
+            title: i.product.title,
+            imageUrl: i.product.imageUrl,
+            price: i.product.price
+          },
+          quantity: i.quantity,
+          totalPrice: i.price
+
+        }
+      })
+    };
+
+    this.orderService.storeOrder(order);
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 }
