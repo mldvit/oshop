5f260e96ea75d8f085855bfab0f2c21b70e8c8c5
Della Vedova Maria Luisa <TD244@tndigit.local>
2020-04-23 17:01:59 +0200

    5- Associating the Order with the Current User

--- src/app/components/check-out/check-out.component.ts
+++ src/app/components/check-out/check-out.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
 import { ShoppingCart } from 'src/app/models/shopping-cart.model';
-import { Subscription } from 'rxjs';
+import { Subscription, forkJoin, combineLatest } from 'rxjs';
 import { OrderService } from 'src/app/services/order.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { map} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-check-out',
@@ -13,15 +16,23 @@ export class CheckOutComponent implements OnInit, OnDestroy {
   shipping = {};
   shoppingCart: ShoppingCart;
   subscription: Subscription;
+  userId;
 
-  constructor(private cartService: CartService, private orderService: OrderService) { }
+  constructor(private authService: AuthService, private cartService: CartService, private orderService: OrderService) { }
 
   ngOnInit() {
-    this.subscription = this.cartService.getCart().subscribe((cart) => this.shoppingCart = cart);
-   }
+    const user$ = this.authService.user$.pipe(map(u =>{ this.userId = u.uid; console.log("uuuuuuu", u);}));
+    const cart$ = this.cartService.getCart().pipe(map(c => this.shoppingCart = c));
+
+    this.subscription = forkJoin({ user$, cart$ })
+      .subscribe((res) => {
+        console.log("Non mi stampa nulla ma funziona--------------", res);
+      });
+  }
 
   placeOrder() {
     const order = {
+      userId: this.userId,
       datePlaced: new Date().getTime(),
       shipping: this.shipping,
       items: this.shoppingCart.items.map(i => {
