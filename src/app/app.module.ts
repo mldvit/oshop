0f007e420129aee1381fe612f11e18295f775812
Della Vedova Maria Luisa <TD244@tndigit.local>
2020-04-30 17:21:45 +0200

    8- Creating the Learning Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { environment } from 'environments/environment';
-import { AuthGuard } from 'shared/services/auth-guard.service';
 import { SharedModule } from 'shared/shared.module';
 import { AdminModule } from './admin/admin.module';
 import { AdminAuthGuard } from './admin/services/admin-auth-guard.service';
@@ -14,13 +13,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './components/bs-navbar/bs-navbar.component';
 import { HomeComponent } from './components/home/home.component';
-import { ObspatternComponent } from './components/learning/obspattern/obspattern.component';
 import { LoginComponent } from './components/login/login.component';
-import { CheckOutComponent } from './shopping/components/check-out/check-out.component';
-import { MyOrdersComponent } from './shopping/components/my-orders/my-orders.component';
-import { OrderSuccessComponent } from './shopping/components/order-success/order-success.component';
+import { ObspatternComponent } from './learning/components/obspattern/obspattern.component';
+import { LearningModule } from './learning/learning.module';
 import { ProductsComponent } from './shopping/components/products/products.component';
-import { ShoppingCartComponent } from './shopping/components/shopping-cart/shopping-cart.component';
 import { ShoppingModule } from './shopping/shopping.module';
 
 
@@ -29,8 +25,7 @@ import { ShoppingModule } from './shopping/shopping.module';
     AppComponent,
     BsNavbarComponent,
     HomeComponent,
-    LoginComponent,
-    ObspatternComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -39,6 +34,7 @@ import { ShoppingModule } from './shopping/shopping.module';
     ShoppingModule,
     AppRoutingModule,
     FormsModule,
+    LearningModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
---
+++ src/app/learning/learning.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ObspatternComponent } from './components/obspattern/obspattern.component';
+
+
+
+@NgModule({
+  declarations: [ObspatternComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class LearningModule { }
